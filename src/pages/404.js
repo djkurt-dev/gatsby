66b02de9ea398f141dbf1652af06932f401f9975
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-12 10:06:23 +0800

    creating layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Layout from "./components/Layout"
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Sorry, that page doesn't exist.</p>
-    </div>
+    <Layout>
+      <div>
+        <h1>404</h1>
+        <p>Sorry, that page doesn't exist.</p>
+      </div>
+    </Layout>
   )
 }
