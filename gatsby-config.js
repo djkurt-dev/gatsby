07712ba4b523c69246ce947be9b0e3c6a0b1fdf2
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-12 18:38:16 +0800

    site metadata & page queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: "Web Warrior",
+    description: "Web Dev Portfolio",
+    copyright: "Copyright (c) 2021 Web Warrior",
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import "../styles/home.css"
 
-export default function Home() {
+export default function Home({ data }) {
+  // console.log("query data: " + data.site.siteMetadata)
+  const { title, description } = data.site.siteMetadata
   return (
     <Layout>
       <section className="header">
@@ -15,7 +17,23 @@ export default function Home() {
             My Portfolio Projects
           </Link>
         </div>
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
+        <p>
+          {title} - {description}
+        </p>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query siteInfo {
+    site(siteMetadata: {}) {
+      siteMetadata {
+        copyright
+        title
+        description
+      }
+    }
+  }
+`
