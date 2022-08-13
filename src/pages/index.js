a924f23051a2ab9a5bfa2a7f473ab833aaf419c1
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-13 12:13:01 +0800

    new description

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ data }) {
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Philippines.</p>
+          <p>Frontend Software Engineer based in Philippines.</p>
           <Link className="btn" to="/projects">
             My Portfolio Projects
           </Link>
